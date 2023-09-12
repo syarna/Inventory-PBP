f9a88f65d954302fccb6a5023747352b8ca96a0c
syarna <syarnasavitri66@gmail.com>
2023-09-12 20:01:05 +0700

    melengkapi

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
+    price = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    
--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Syarna Savitri',
+        'class': 'PBP B',
+
+        'barang': 'Palu Thor',
+        'amount': '1',
+        'description': 'Limited edition dari Asgard',
+        'price':'1.000.000 Krona',
+    }
+
+    return render(request, "main.html", context)
