488445a362db74332be3a81f16ceafaee4d05de9
syarna <syarnasavitri66@gmail.com>
2023-09-20 09:04:13 +0700

    menyelesaikan tugas 3

--- main/models.py
+++ main/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
     description = models.TextField()
     price = models.IntegerField()
     date_added = models.DateField(auto_now_add=True)
