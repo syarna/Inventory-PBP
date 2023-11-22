334ff59f1c9b26b8354a293d10fdad9e8774baef
syarna <syarnasavitri66@gmail.com>
2023-11-22 12:05:06 +0700

    fixing minor error

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import logout as auth_logout
 
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
+    
 @csrf_exempt
 def login(request):
     username = request.POST['username']
